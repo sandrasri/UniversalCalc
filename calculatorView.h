db991898207086111a33c42cf63af33d16747d80
sandrasri <156359768+sandrasri@users.noreply.github.com>
2025-04-17 16:03:42 -0700

    Create calculatorView.h

---
+++ calculatorView.h
@@ -0,0 +1,11 @@
+// calculatorView.h
+#ifndef CALCULATOR_VIEW_H
+#define CALCULATOR_VIEW_H
+
+#include <gtk/gtk.h>
+
+GtkWidget* create_calculator_view(); // instead of void init_view()
+
+#endif
+
+
