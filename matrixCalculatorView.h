aa3faff797ed6611829cbb11e58cc1b97a2b02e9
sandrasri <156359768+sandrasri@users.noreply.github.com>
2025-04-17 16:05:10 -0700

    Create matrixCalculatorView.h

---
+++ matrixCalculatorView.h
@@ -0,0 +1,9 @@
+// matrixCalculatorView.h
+#ifndef MATRIX_CALCULATOR_VIEW_H
+#define MATRIX_CALCULATOR_VIEW_H
+
+#include <gtk/gtk.h>
+
+GtkWidget* create_matrix_calculator_view();  // future stub
+
+#endif
