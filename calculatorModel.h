8bf6765430f04fc22d131c256434a185e5b989e7
sandrasri <156359768+sandrasri@users.noreply.github.com>
2025-04-17 16:05:54 -0700

    Create calculatorModel.h

---
+++ calculatorModel.h
@@ -0,0 +1,15 @@
+// model.h
+#ifndef MODEL_H
+#define MODEL_H
+
+// Declare functions for performing calculations
+double add(double a, double b);
+double subtract(double a, double b);
+double multiply(double a, double b);
+double divide(double a, double b);
+
+// Functions to set and get the current value
+void set_current_value(double value);
+double get_current_value();
+
+#endif
