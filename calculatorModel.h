a7e5d1934b9dbef26d50df6353d0ebbaa4ad6326
sandrasri <156359768+sandrasri@users.noreply.github.com>
2025-04-18 16:11:12 -0700

    Update calculatorModel.h

--- calculatorModel.h
+++ calculatorModel.h
@@ -2,14 +2,36 @@
 #ifndef MODEL_H
 #define MODEL_H
 
-// Declare functions for performing calculations
+// Simple Calculator
 double add(double a, double b);
 double subtract(double a, double b);
 double multiply(double a, double b);
 double divide(double a, double b);
-
-// Functions to set and get the current value
+double modulus(double a, double b);
+double exponent(double a, double b);
+double factorial (double a);
+double sine(double a);
+double cosine(double a);
+double tangent (double a);
+double logarithm(double a);
+double naturalLog(double a);
+double logBase(double a, double b);
+double absoluteValue(double a);
+double squareRoot(double a);
+double squareRootWBase(double a, double b);
 void set_current_value(double value);
 double get_current_value();
+
+//Matrix Calculator
+void addition_matrix(int matrixA[3][3], int matrixB[3][3], int matrixAns[3][3]);
+void subtraction_matrix(int matrixA[3][3], int matrixB[3][3], int matrixAns[3][3]);
+void multiplication_matrix(int matrixA[3][3], int matrixB[3][3], int matrixAns[3][3]);
+void scalar_matrix(int matrixBase[3][3], int scalar, double matrixSecAns[3][3]);
+void inverse_matrix(int matrixBase[3][3],double matrixSecAns[3][3]);
+void transpose_matrix(int matrixBase[3][3], double matrixSecAns[3][3]);
+
+// Conversion Calculator
+double conversion(char conversion[50], char og_conversion[50], double value);
+
 
 #endif
