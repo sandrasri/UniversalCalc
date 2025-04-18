0268b0f7f35c6a73a67a9588d72a3ee80e8f99ce
sandrasri <156359768+sandrasri@users.noreply.github.com>
2025-04-18 16:12:22 -0700

    Update controller.c

--- controller.c
+++ controller.c
@@ -2,9 +2,10 @@
 #include "controller.h"
 #include "calculatorView.h"
 #include "matrixCalculatorView.h"
+#include "conversionView.h"
 #include <gtk/gtk.h>
 
-GtkWidget *stack;  // shared stack for view switching
+extern GtkWidget *stack;  // shared stack for view switching
 
 void on_button_basic_clicked(GtkButton *button, gpointer user_data) {
     gtk_stack_set_visible_child_name(GTK_STACK(stack), "basic");
@@ -14,11 +15,15 @@ void on_button_matrix_clicked(GtkButton *button, gpointer user_data) {
     gtk_stack_set_visible_child_name(GTK_STACK(stack), "matrix");
 }
 
+void on_button_conversion_clicked(GtkButton *button, gpointer user_data) {
+    gtk_stack_set_visible_child_name(GTK_STACK(stack), "conversion");
+}
+
 void activate(GtkApplication *app, gpointer user_data) {
     GtkWidget *window;
     GtkWidget *main_box;
     GtkWidget *main_menu;
-    GtkWidget *button_basic, *button_matrix;
+    GtkWidget *button_basic, *button_matrix, *button_conversion;
 
     window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "Calculator Launcher");
@@ -37,9 +42,11 @@ void activate(GtkApplication *app, gpointer user_data) {
 
     button_basic = gtk_button_new_with_label("Basic Calculator");
     button_matrix = gtk_button_new_with_label("Matrix Calculator");
+    button_conversion = gtk_button_new_with_label("Conversion Calculator");
 
     gtk_box_pack_start(GTK_BOX(main_menu), button_basic, FALSE, FALSE, 0);
     gtk_box_pack_start(GTK_BOX(main_menu), button_matrix, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(main_menu), button_conversion, FALSE, FALSE, 0);
 
     gtk_stack_add_named(GTK_STACK(stack), main_menu, "menu");
 
@@ -50,9 +57,13 @@ void activate(GtkApplication *app, gpointer user_data) {
     GtkWidget *matrix_view = create_matrix_calculator_view();
     gtk_stack_add_named(GTK_STACK(stack), matrix_view, "matrix");
 
+    GtkWidget *conversion_view = create_conversion_calculator_view();
+    gtk_stack_add_named(GTK_STACK(stack), conversion_view, "conversion");
+
     // Signal connections
     g_signal_connect(button_basic, "clicked", G_CALLBACK(on_button_basic_clicked), NULL);
     g_signal_connect(button_matrix, "clicked", G_CALLBACK(on_button_matrix_clicked), NULL);
+    g_signal_connect(button_conversion, "clicked", G_CALLBACK(on_button_conversion_clicked), NULL);
 
     gtk_stack_set_visible_child_name(GTK_STACK(stack), "menu");  // start with menu
     gtk_widget_show_all(window);
