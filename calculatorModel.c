590857bef40c68143141c5c1074ed0dc073c1e83
sandrasri <156359768+sandrasri@users.noreply.github.com>
2025-04-21 16:23:19 -0700

    Update calculatorModel.c

--- calculatorModel.c
+++ calculatorModel.c
@@ -5,8 +5,12 @@
 #include <math.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdbool.h>
 #define _USE_MATH_DEFINES
-#define NUM_CONVERSIONS 14
+#define NUM_HEXA 16
+#define NUM_CONVERSIONS 34
+#define MAX_UNIT_LEN 50
+#define MAX_VISITED 50
 
 typedef struct {
     char key[50];
@@ -14,20 +18,59 @@ typedef struct {
 } KeyValuePair;
 
 KeyValuePair conversionChart[NUM_CONVERSIONS] = {
-    {"kg_to_lb", 2.20462},
-    {"lb_to_kg", 0.45359},
-    {"m_to_ft", 3.28084},
-    {"ft_to_m", 0.3048},
-    {"cm_to_in", 0.393701},
-    {"in_to_cm", 2.54},
-    {"km_to_miles", 0.621371},
-    {"miles_to_km", 1.60934},
-    {"g_to_oz", 0.035274},
-    {"oz_to_g", 28.3495},
-    {"l_to_gal", 0.264172},
-    {"gal_to_l", 3.78541},
-    {"c_to_f", 1.8},
-    {"f_to_c", 0.555556},
+    {"kilogram_to_lb", 2.20462},
+    {"lb_to_kilogram", 0.45359},
+    {"meter_to_feet", 3.28084},
+    {"feet_to_meter", 0.3048},
+    {"centimeter_to_inches", 0.393701},
+    {"inches_to_centimeter", 2.54},
+    {"kilometer_to_miles", 0.621371},
+    {"miles_to_kilometer", 1.60934},
+    {"gram_to_ounce", 0.035274},
+    {"ounce_to_gram", 28.3495},
+    {"litre_to_gallon", 0.264172},
+    {"gallon_to_litre", 3.78541},
+    {"celsius_to_fahrenheit", 1.8},
+    {"fahrenheit_to_celsius", 0.555556},
+    {"lb_to_ounce", 16.0},
+    {"ounce_to_lb", 0.0625},
+    {"kilogram_to_gram", 1000.0},
+    {"gram_to_kilogram", 0.001},
+    {"meter_to_centimeter", 100.0},
+    {"centimeter_to_meter", 0.01},
+    {"feet_to_inches", 12.0},
+    {"inches_to_feet", 0.0833333},
+    {"kilometer_to_meter", 1000.0},
+    {"meter_to_kilometer", 0.001},
+    {"mile_to_feet", 5280.0},
+    {"feet_to_mile", 0.000189394},
+    {"litre_to_millilitre", 1000.0},
+    {"millilitre_to_litre", 0.001},
+    {"gallon_to_ounce", 128.0},
+    {"ounce_to_gallon", 0.0078125},
+    {"celsius_to_kelvin", 273.15},
+    {"kelvin_to_celsius", -273.15},
+    {"fahrenheit_to_kelvin", 255.927778},
+    {"kelvin_to_fahrenheit", -457.87}
+};
+
+KeyValuePair hexaConversion[NUM_HEXA] = {
+    {"0", 0},
+    {"1", 1},
+    {"2", 2},
+    {"3", 3},
+    {"4", 4},
+    {"5", 5},
+    {"6", 6},
+    {"7", 7},
+    {"8", 8},
+    {"9", 9},
+    {"A", 10},
+    {"B", 11},
+    {"C", 12},
+    {"D", 13},
+    {"E", 14},
+    {"F", 15},
 };
 
 // This will store the current value
@@ -266,20 +309,96 @@ void transpose_matrix(int matrixBase[3][3], double matrixSecAns[3][3]) {
     }
 }
 
-//Unit Conversion Features
-double conversion(char conversion[50], char og_conversion[50], double value) {
-    char conversion_string[100];
-    sprintf(conversion_string, "%s_to_%s", conversion, og_conversion);
+// Recursive DFS to find a path
+bool dfs(const char *from, const char *to, double value, double *result, char visited[MAX_VISITED][MAX_UNIT_LEN], int depth) {
+    if (depth >= MAX_VISITED) return false;
+
+    for (int i = 0; i < depth; i++) {
+        if (strcasecmp(visited[i], from) == 0)
+            return false; // already visited
+    }
+    strcpy(visited[depth], from);
+
+    char key[100];
+    sprintf(key, "%s_to_%s", from, to);
     for (int i = 0; i < NUM_CONVERSIONS; i++) {
-        if (strcasecmp(conversionChart[i].key, conversion_string) == 0) {
-            if (strcmp(conversion, "f") == 0 && strcmp(og_conversion, "c") == 0) {
-                return ((value - 32) * conversionChart[i].value);
-            } else if (strcmp(conversion, "c") == 0 && strcmp(og_conversion, "f") == 0) {
-                return ((conversionChart[i].value * value) + 32);
-            } else {
-                return conversionChart[i].value * value;
-            }
+        if (strcasecmp(conversionChart[i].key, key) == 0) {
+            *result = value * conversionChart[i].value;
+            return true;
         }
     }
-    return 0.0;
+
+    // Try intermediate paths
+    for (int i = 0; i < NUM_CONVERSIONS; i++) {
+        char intermediate_from[MAX_UNIT_LEN], intermediate_to[MAX_UNIT_LEN];
+        sscanf(conversionChart[i].key, "%[^_]", intermediate_from);
+        sscanf(conversionChart[i].key, "%*[^_]_to_%s", intermediate_to);
+
+        if (strcasecmp(intermediate_from, from) == 0) {
+            double new_value = value * conversionChart[i].value;
+            if (dfs(intermediate_to, to, new_value, result, visited, depth + 1))
+                return true;
+        }
+    }
+
+    return false;
+}
+
+double perform_conversion(const char *from, const char *to, double value) {
+    double result = 0.0;
+    char visited[MAX_VISITED][MAX_UNIT_LEN] = {0};
+
+    if (dfs(from, to, value, &result, visited, 0)) {
+        return result;
+    } else {
+        return 0.0;
+    }
+}
+
+// Binary and Hexadecimal conversions
+char* compute_numconversion(const char *from, const char *to, int number) {
+    if (strcmp(to, "binary") == 0) {
+        if (number == 0) {
+           return "0.0";
+        }
+        int binary[32];
+        int i = 0;
+        while (number > 0) {
+            binary[i] = number % 2;
+            number = number / 2;
+            i++;
+        }
+        
+        char* binaryString = malloc(50 * sizeof(char));
+        binaryString[0] = '\0'; // Initialize the string
+        char temp[2];
+        // Append binary digits to the string
+        for (int j = i - 1; j >= 0; j--) {
+            sprintf(temp, "%d", binary[j]);
+            strcat(binaryString, temp);
+        }
+        return binaryString;
+    free(binaryString);
+    } else if (strcmp(to, "hexadecimal") == 0) {
+        int quotient = number;
+        char* answer = malloc(50 * sizeof(char));
+        answer[0] = '\0';
+        double remainder = 0;
+        while (quotient != 0) {
+            remainder = quotient % 16;
+            char temp[2];
+            //answer = realloc(answer, (strlen(answer) + 1) * sizeof(char));
+            for (int i = 0; i < NUM_HEXA; i++) {
+                if(hexaConversion[i].value == remainder) {
+                    strcpy(temp, hexaConversion[i].key);
+                }
+            }
+            memmove(answer + 1, answer, strlen(answer) + 1);
+            answer[0] = temp[0];
+            quotient = quotient / 16;
+        } 
+        return answer;
+    free(answer);
+    }
+    return "0.0";
 }
